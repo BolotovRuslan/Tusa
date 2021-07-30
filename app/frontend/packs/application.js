e1827d3f4dc994c8a4adc60c6b24112f5dbe3ce3
bolotovruslan <lancer11.com@gmail.com>
2021-07-30 16:47:12 +0300

    events add

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,9 +1,13 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
 import "../styles/application"
+
+import "bootstrap/dist/js/bootstrap"
 
 Rails.start()
 ActiveStorage.start()
 
 const images = require.context('../images', true)
+
