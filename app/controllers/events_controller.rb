711dd9be5f37b98fc585b3081a6841e4848e5c6c
bolotovruslan <lancer11.com@gmail.com>
2021-08-02 13:54:28 +0300

    views events change | avatar user add

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_event, only: [:show]
+  before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
   def index
     @events = Event.all
@@ -9,14 +11,14 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
   def edit
   end
 
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
@@ -50,11 +52,16 @@ class EventsController < ApplicationController
   end
 
   private
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    def event_params
-      params.require(:event).permit(:title, :address, :datetime, :description)
-    end
+  def set_current_user_event
+      @event = current_user.events.find(params[:id])
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :address, :datetime, :description)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,9 @@ module ApplicationHelper
     end
     nil
   end
+
+  def user_avatar(user)
+    asset_pack_path('media/images/user.png')
+  end
+
 end
