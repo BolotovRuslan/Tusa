9e2c69d420df0ce84925389597e9d33c9ab7596f
bolotovruslan <lancer11.com@gmail.com>
2021-08-02 14:04:46 +0300

    users controller and views show change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update ]
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_current_user, except: [:show]
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -20,11 +22,12 @@ class UsersController < ApplicationController
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
+  
+  def set_current_user
+    @user = current_user
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
